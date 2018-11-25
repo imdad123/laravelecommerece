2d1139321dbbed35e4ac128ac93c39e3ddccc37b
imdad <uimdad19@gmail.com>
2018-11-25 22:20:28 +0500

    model and controller added

---
+++ database/migrations/2018_11_25_165350_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("user_id");
+            $table->integer("product_id");
+            $table->integer("qty");
+            $table->string("status");
+            $table->double("price");
+            $table->integer("payment_id");
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
